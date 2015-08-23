dedffa1aa22c64da53cf3b644c58f597e9efe384
jrizzoli <joey@cyanogenmoditalia.it>
2015-08-23 17:41:41 +0200

    berkeley: releasetools: Run resize2fs on /system partition
    * This basically will allow people to use
      whole 5.5G of /system.
    
    Change-Id: I6877347ec2d4af20b1afe708241fe06403e293bd
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- device.mk
+++ device.mk
@@ -95,6 +95,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Recovery
+PRODUCT_PACKAGES += \
+    resize2fs_static
+
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -17,3 +17,8 @@ def FullOTA_InstallEnd(info):
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/genfscon exfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/genfscon fuseblk/d", "/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('unmount("/system");');
+
+def FullOTA_PostValidate(info):
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/bootdevice/by-name/system");');
+    info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/bootdevice/by-name/system");');
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/bootdevice/by-name/system");');
