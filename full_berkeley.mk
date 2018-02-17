6e5c12045ff06262ba97490c11e359556605fe2b
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-17 17:11:12 +0100

    berkeley: Include treble common mk

--- full_berkeley.mk
+++ full_berkeley.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
 
 # Inherit from berkeley device
 $(call inherit-product, device/huawei/berkeley/device.mk)
