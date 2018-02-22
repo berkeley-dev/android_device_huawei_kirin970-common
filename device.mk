458574a561188fc1fa98ae492c2835d547095858
Huawei-Dev <surdu_petru@yahoo.com>
2018-02-22 23:47:44 +0100

    disable sdcardfs for moment to fix some external sdcard issues

--- device.mk
+++ device.mk
@@ -85,3 +85,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vndk-compat/ld.config.compat.txt:system/etc/ld.config.compat.txt \
     $(LOCAL_PATH)/vndk-compat/vndk-compat.rc:system/etc/init/vndk-compat.rc
+
+# disable sdcardfs
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.sys.sdcardfs=0 \
+    persist.sys.sdcardfs.emulated=0 \
+    persist.sys.sdcardfs.public=0
