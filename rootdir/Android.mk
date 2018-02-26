842e4bfde582be7704003e05d809ae09a995bca5
LuK1337 <priv.luk@gmail.com>
2018-02-26 13:34:11 +0100

    berkeley: Discard more Huawei's changes
    * TODO: Get rid of hacked init.usb.* imports.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,5 +5,5 @@ LOCAL_MODULE       := init.kirin970.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.kirin970.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
