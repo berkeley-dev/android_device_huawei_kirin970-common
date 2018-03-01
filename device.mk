4da4c990905801b133d971ed287823f1218caa2d
LuK1337 <priv.luk@gmail.com>
2018-03-01 19:26:22 +0100

    bkl: Implement proper VNDK detection * Apparently US model is shipping with Android 8.1 so we need to support it as well.

--- device.mk
+++ device.mk
@@ -94,8 +94,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vndk-compat/vndk-detect:system/bin/vndk-detect \
     $(LOCAL_PATH)/vndk-compat/ld.config.compat.txt:system/etc/ld.config.compat.txt \
     $(LOCAL_PATH)/vndk-compat/vndk-compat.rc:system/etc/init/vndk-compat.rc
+
+PRODUCT_PACKAGES += \
+    vndk-sp
 
 # disable sdcardfs
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
