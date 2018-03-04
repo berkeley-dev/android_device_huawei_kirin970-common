ca7783b82c57a5a023bb89d5c1b57bf2c28b3428
Pierre-Hugues Husson <phh@phh.me>
2018-03-04 21:33:55 +0100

    berkeley: rootdir: Set environment variables during build time

--- device.mk
+++ device.mk
@@ -31,12 +31,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Device init scripts
 PRODUCT_PACKAGES += \
-    init.kirin970.rc
-
-ifeq ($(TARGET_PRODUCT),lineage_berkeley)
-PRODUCT_PACKAGES += \
-    init.kirin970.lineage.rc
-endif
+    init.kirin970.rc \
+    init.kirin970.environ.rc
 
 # Display
 PRODUCT_PACKAGES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,9 +9,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.kirin970.lineage.rc
+LOCAL_MODULE       := init.kirin970.environ.rc
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.kirin970.lineage.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
+LOCAL_POST_INSTALL_CMD := \
+    mkdir -p $(PRODUCT_OUT)/system/etc/init && \
+    sed -e 's?%BOOTCLASSPATH%?$(PRODUCT_BOOTCLASSPATH)?g' $(LOCAL_PATH)/etc/init.kirin970.environ.rc > $(PRODUCT_OUT)/system/etc/init/init.kirin970.environ.rc && \
+    sed -i -e 's?%SYSTEMSERVERCLASSPATH%?$(PRODUCT_SYSTEM_SERVER_CLASSPATH)?g' $(PRODUCT_OUT)/system/etc/init/init.kirin970.environ.rc
+include $(BUILD_PHONY_PACKAGE)
