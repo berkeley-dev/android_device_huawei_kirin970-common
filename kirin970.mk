4fb9fa4ef53112315eed910e6ebeed7954682909
Pierre-Hugues Husson <phh@phh.me>
2018-04-01 00:21:03 +0200

    kirin970-common: Override vendor's usb_audio_policy_configuration
    Android 8.1 added the support for new type of USB Audio devices
    (USB_HEADSET, instead of old USB_DEVICE)
    Some older USB devices got converted to this USB_HEADSET while still
    using the old protocol.
    There is no driver change needed, but the audio policy needs to be
    changed.
    Since vendors are simply using AOSP usb_audio_policy_configuration
    Override it by ourself.

--- kirin970.mk
+++ kirin970.mk
@@ -21,6 +21,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Audio
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
+
 # Device init scripts
 PRODUCT_PACKAGES += \
     init.kirin970.rc \
