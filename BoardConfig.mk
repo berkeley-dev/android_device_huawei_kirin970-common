8c08d990d3ec623f77251864ebe5ee1e75d2c70b
LuK1337 <priv.luk@gmail.com>
2018-04-20 14:04:26 +0200

    berkeley: Import fstab.kirin970

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,5 +48,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3707764736
 # Properties
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.kirin970
+
 # SELinux
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
