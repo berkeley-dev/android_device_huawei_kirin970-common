313b0786d175bc14105d2bd2e088c7737f9f9555
LuK1337 <priv.luk@gmail.com>
2018-04-20 16:23:18 +0200

    berkeley: Add initial release tools

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,5 +59,8 @@ TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.kirin970
 
+# Release tools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
+
 # SELinux
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
