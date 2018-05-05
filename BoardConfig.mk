2f701c90fa6fc6e92bae16a4127279e32fe2f3ac
LuK1337 <priv.luk@gmail.com>
2018-05-05 16:10:40 +0200

    berkeley: Add blobs for displayengineserver 1.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,5 +69,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib64/libdisplayengineservice.so|libshims_hwsmartdisplay_jni.so \
+    /system/lib64/libdisplayenginesvc_1_0.so|libshims_hwsmartdisplay_jni.so \
+    /system/lib64/libdisplayenginesvc_1_1.so|libshims_hwsmartdisplay_jni.so \
     /system/lib64/libhwsmartdisplay_jni.so|libshims_hwsmartdisplay_jni.so
