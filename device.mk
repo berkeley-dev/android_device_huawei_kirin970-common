05bc9fe6e4027b26cb6f045484fa531bb3a3ed75
LuK1337 <priv.luk@gmail.com>
2018-05-08 02:07:27 +0200

    berkeley: Set ro.cust.cdrom to /dev/null
    * This basically kills stupid HiSuite CD
      which is totally useless on AOSP based ROMs.

--- device.mk
+++ device.mk
@@ -85,7 +85,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.codename=$(PLATFORM_VERSION_CODENAME) \
     ro.build.version.huawei=8.0.0 \
     ro.build.version.release=$(PLATFORM_VERSION) \
-    ro.build.version.sdk=$(PLATFORM_SDK_VERSION)
+    ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
+    ro.cust.cdrom=/dev/null
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
