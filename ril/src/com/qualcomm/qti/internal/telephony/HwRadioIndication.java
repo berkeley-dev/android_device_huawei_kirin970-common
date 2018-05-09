ff40dbb5e34d1146937f7ebf3cbe760cd2c34558
Paul Keith <javelinanddart@gmail.com>
2018-05-09 20:49:24 +0200

    berkeley: Implement dummy custom RIL
    * To implement signal strength hacks
    
    This reverts commit 78f050bfc14a6808639357627396013a4b7f7cc3.

---
+++ ril/src/com/qualcomm/qti/internal/telephony/HwRadioIndication.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.qualcomm.qti.internal.telephony;
+
+import android.os.AsyncResult;
+import android.telephony.SignalStrength;
+
+import com.android.internal.telephony.RIL;
+import com.android.internal.telephony.RadioIndication;
+
+import com.qualcomm.qti.internal.telephony.HwRIL;
+
+public class HwRadioIndication extends RadioIndication {
+
+    HwRIL mHwRil;
+
+    public HwRadioIndication(RIL ril) {
+        super(ril);
+        if (ril instanceof HwRIL) {
+           mHwRil = (HwRIL) ril;
+        }
+    }
+
+    @Override
+    public void currentSignalStrength(int indicationType,
+                                      android.hardware.radio.V1_0.SignalStrength signalStrength) {
+        if (mHwRil != null) {
+            mHwRil.processInd(indicationType);
+
+            SignalStrength ss = HwRIL.convertHalSignalStrength(signalStrength);
+            // Note this is set to "verbose" because it happens frequently
+            // This is always false, so just comment it out
+            //if (RIL.RILJ_LOGV) mHwRil.unsljLogvRet(RIL_UNSOL_SIGNAL_STRENGTH, ss);
+
+            if (mHwRil.getSignalStrengthRegistrant() != null) {
+                mHwRil.getSignalStrengthRegistrant().notifyRegistrant(new AsyncResult (null, ss, null));
+            }
+        } else {
+            super.currentSignalStrength(indicationType, signalStrength);
+        }
+    }
+
+}
---
+++ ril/src/com/qualcomm/qti/internal/telephony/HwRadioResponse.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.qualcomm.qti.internal.telephony;
+
+import android.hardware.radio.V1_0.RadioError;
+import android.hardware.radio.V1_0.RadioResponseInfo;
+import android.os.AsyncResult;
+import android.os.Message;
+import android.telephony.SignalStrength;
+
+import com.android.internal.telephony.RIL;
+import com.android.internal.telephony.RILRequest;
+import com.android.internal.telephony.RadioResponse;
+
+import com.qualcomm.qti.internal.telephony.HwRIL;
+
+public class HwRadioResponse extends RadioResponse {
+
+    HwRIL mHwRil;
+
+    public HwRadioResponse(RIL ril) {
+        super(ril);
+        if (ril instanceof HwRIL) {
+           mHwRil = (HwRIL) ril;
+        }
+    }
+
+    /**
+     * Helper function to send response msg
+     * @param msg Response message to be sent
+     * @param ret Return object to be included in the response message
+     */
+    static void sendMessageResponse(Message msg, Object ret) {
+        if (msg != null) {
+            AsyncResult.forMessage(msg, ret, null);
+            msg.sendToTarget();
+        }
+    }
+
+    @Override
+    public void getSignalStrengthResponse(RadioResponseInfo responseInfo,
+                                          android.hardware.radio.V1_0.SignalStrength sigStrength) {
+        if (mHwRil != null) {
+            responseSignalStrength(responseInfo, sigStrength);
+        } else {
+            super.getSignalStrengthResponse(responseInfo, sigStrength);
+        }
+    }
+
+    private void responseSignalStrength(RadioResponseInfo responseInfo,
+                                        android.hardware.radio.V1_0.SignalStrength sigStrength) {
+        RILRequest rr = mHwRil.processResp(responseInfo);
+
+        if (rr != null) {
+            SignalStrength ret = HwRIL.convertHalSignalStrength(sigStrength);
+            if (responseInfo.error == RadioError.NONE) {
+                sendMessageResponse(rr.getResult(), ret);
+            }
+            mHwRil.processRespDone(rr, responseInfo, ret);
+        }
+    }
+
+}
