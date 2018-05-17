ae2d45d5fbdcae5fb439e0edfa0e1a0066fabf0c
LuK1337 <priv.luk@gmail.com>
2018-05-17 11:07:31 +0200

    kirin970-common: bluetooth: Add support for multiple devices

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,24 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Honor View 10"
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char *BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+    if (strcmp(product_device, "HWBKL") == 0)
+        return "Honor View 10";
+    if (strcmp(product_device, "HWCLT") == 0)
+        return "P20 Pro";
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BTM_BYPASS_EXTRA_ACL_SETUP TRUE
 
 #define BLE_INCLUDED TRUE
