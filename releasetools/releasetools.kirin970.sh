efb9037a82c7d7b1f1a6c5858ec028385a88a2a7
LuK1337 <priv.luk@gmail.com>
2018-06-18 20:45:17 +0200

    kirin970-common: Disable parsing intra-refresh-mode parameter in libstagefright for 8.0 vendor
    * Our OMX doesn't like setting OMX_IndexParamVideoIntraRefresh
      parameter and returns non-OK value therefore breaking wireless
      display (with real TVs).
    * Vaguely based on https://github.com/phhusson/platform_frameworks_av/pull/1

--- releasetools/releasetools.kirin970.sh
+++ releasetools/releasetools.kirin970.sh
@@ -118,6 +118,10 @@ if [ "$(grep ro.build.version.release /vendor/build.prop)" = "ro.build.version.r
     # Copy over vendor media_codecs.xml and disable unwanted HW codecs
     cp /vendor/etc/media_codecs.xml /system/etc/media_codecs.xml
     sed -i "s/<MediaCodec name=\"OMX.hisi.video.decoder.avc\" type=\"video\/avc\" >/<MediaCodec name=\"OMX.hisi.video.decoder.avc\" type=\"video\/no-avc\" >/g" /system/etc/media_codecs.xml
+
+    # Disable parsing intra-refresh-mode parameter in libstagefright
+    sed -i 's/intra-refresh-mode/intra-refresh-nope/' /system/lib64/libstagefright.so
+    sed -i 's/intra-refresh-mode/intra-refresh-nope/' /system/lib/libstagefright.so
 fi
 
 exit 0
