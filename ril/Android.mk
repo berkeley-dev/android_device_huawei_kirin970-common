00beefec18b963ba45bc92d6bae3ea0ce88a85cb
chrmhoffmann <chrmhoffmann@gmail.com>
2018-07-15 13:46:05 +0200

    kirin970-common: Actually pull in telephony-ext into final build
    * Avoids errors in the logcat
    
    Change-Id: I532f114a3b788e5a6f8837eef6dce4c8337fa474

--- ril/Android.mk
+++ ril/Android.mk
@@ -20,6 +20,7 @@ ifneq ($(TARGET_PROVIDES_QTI_TELEPHONY_JAR),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := qti-telephony-common
 LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
+LOCAL_REQUIRED_MODULES := telephony-ext
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
