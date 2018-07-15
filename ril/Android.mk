0d0e335909374a2917cc2335fa998a0ac4bd3542
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-15 13:10:36 -0700

    kirin970: ril: Build and include vendor.huawei.hardware.radio-V1.1-java-static

--- ril/Android.mk
+++ ril/Android.mk
@@ -20,6 +20,7 @@ ifneq ($(TARGET_PROVIDES_QTI_TELEPHONY_JAR),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := qti-telephony-common
 LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
+LOCAL_STATIC_JAVA_LIBRARIES := vendor.huawei.hardware.radio-V1.1-java-static
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
